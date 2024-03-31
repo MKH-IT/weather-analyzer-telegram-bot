71a6a943f58bf6f72aba03f13289000b285970a2
Rustam Zokirov <zokirovrustam202@gmail.com>
2024-03-31 11:47:32 +0500

    Implemented new logic for language, location and phone number

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from decouple import config
 from telebot import types
 
 from database import JSONDatabase, Database
-from helpers import gen_languages_markup
 
 TELEGRAM_API_TOKEN = config("TELEGRAM_API_TOKEN")
 
@@ -12,55 +11,59 @@ bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 
 @bot.message_handler(commands=["start"])
 def start(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup.add(types.KeyboardButton("English 🇬🇧"), types.KeyboardButton("Russian 🇷🇺"))
     bot.send_message(
         message.chat.id,
-        f"Hello! {message.from_user.first_name}!",
-        reply_markup=gen_languages_markup(),
+        "Please select a language:",
+        reply_markup=markup,
     )
-    bot.register_next_step_handler(message, on_click_language)
 
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith("language_"))
-def on_click_language(call):
-    if call.data == "language_english":
-        bot.answer_callback_query(call.id, "You selected English language.")
-    elif call.data == "language_russian":
-        bot.answer_callback_query(call.id, "You selected Russian language.")
-    elif call.data == "language_uzbek":
-        bot.answer_callback_query(call.id, "You selected Uzbek language.")
-    ask_for_location(call.message.chat.id)
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.text == "English 🇬🇧":
+        ask_location(message)
+    elif message.text == "Russian 🇷🇺":
+        ask_location(message)
+    else:
+        bot.send_message(message.chat.id, "Please select a valid language.")
 
 
-def ask_for_location(chat_id):
-    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    location_button = types.KeyboardButton(text="Send Location", request_location=True)
-    markup.add(location_button)
-    bot.send_message(chat_id, "Please share your location:", reply_markup=markup)
-    bot.register_next_step_handler_by_chat_id(chat_id, process_location)
+def ask_location(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup.add(types.KeyboardButton("Share Location", request_location=True))
+    bot.send_message(
+        message.chat.id, "Please share your location:", reply_markup=markup
+    )
 
 
-def process_location(message):
-    location = message.location
-    print(location)
-    # Do something with the location data
-    ask_for_phone_number(message.chat.id)
+@bot.message_handler(content_types=["location"])
+def handle_location(message):
+    print(message)
+    ask_phone_number(message)
 
 
-def ask_for_phone_number(chat_id):
-    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+def ask_phone_number(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     phone_number_button = types.KeyboardButton(
         text="Send phone number", request_contact=True
     )
     markup.add(phone_number_button)
-    bot.send_message(chat_id, "Please share your phone number:", reply_markup=markup)
-    bot.register_next_step_handler_by_chat_id(chat_id, process_phone_number)
+    bot.send_message(
+        message.chat.id, "Please send your phone number:", reply_markup=markup
+    )
 
 
-def process_phone_number(message):
-    phone_number = message.text
-    print(phone_number)
-    # Do something with the phone number
-    bot.send_message(message.chat.id, "Thank you for sharing your information!")
+@bot.message_handler(content_types=["contact"])
+def handle_contact(message):
+    print(message)
+    remove_keyboard = types.ReplyKeyboardRemove(selective=False)
+    bot.send_message(
+        message.chat.id,
+        "Thank you! Your phone number has been recorded.",
+        reply_markup=remove_keyboard,
+    )
 
 
 def main():
